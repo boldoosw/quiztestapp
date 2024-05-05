cdf3283c355ea4cf783ff212fa1e20f3dc1d5ed8
ganbold <{ID}+{boldoosw@gmail.com}@users.noreply.github.com>
2024-05-05 12:31:41 +0800

    chartss

--- src/app/matrix_test/page.jsx
+++ src/app/matrix_test/page.jsx
@@ -1,19 +1,23 @@
 "use client";
+import React, { useRef } from "react";
 import ClimovChart from "@/components/Charts/ClimovChart";
 import HollandChart from "@/components/Charts/HollandChart";
 import MatrixChart from "@/components/Charts/MatrixChart";
-import React from "react";
+import { useSearchParams } from "next/navigation";
 
-function page() {
+const MatrixTest = () => {
+  const searchParams = useSearchParams();
+
+  const email = searchParams.get("email");
   return (
     <div>
       <div className="mt-4  grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ClimovChart />
-        <HollandChart />
+        <ClimovChart email={email} />
+        <HollandChart email={email} />
       </div>
       <MatrixChart />
     </div>
   );
-}
+};
 
-export default page;
+export default MatrixTest;
