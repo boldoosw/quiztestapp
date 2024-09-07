8e70c017ff99ba495b3faf781a92103017648233
ganbold <{ID}+{boldoosw@gmail.com}@users.noreply.github.com>
2024-09-07 22:29:49 +0800

    dashboardone two deer contact png nemlees

--- src/app/dashboard_two/page.jsx
+++ src/app/dashboard_two/page.jsx
@@ -67,13 +67,13 @@ const DashboardTwo = () => {
           <MChart email={email} />
           {/* <MatrixChart /> */}
         </div>
+        <div className="flex flex-col  bg-white justify-center py-6 px-10 items-center rounded-tr-[35px] rounded-bl-[35px] shadow-2xl md:min-h-[340px] w-full card-item-div  min-h-[260px]">
+          <Image className="h-[80px] sm:h-full " src={contactus} alt="logo" />
+          <div className="mt-4"></div>
+        </div>
         <div className="bg-[#a03043] text-white text-center  p-4 text-[14px] mt-8  grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
           <TopicInfo topic_id={topic_id} role={role} />
         </div>
-      </div>
-      <div className="flex flex-col  bg-white justify-center py-6 px-10 items-center rounded-tr-[35px] rounded-bl-[35px] shadow-2xl md:min-h-[340px] w-full card-item-div  min-h-[260px]">
-        <Image className="h-[80px] sm:h-full " src={contactus} alt="logo" />
-        <div className="mt-4"></div>
       </div>
     </main>
   );
