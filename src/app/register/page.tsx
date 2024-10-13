10dfafe50eb677dca7f97955eb5bd841b4b36a83
boldoosw <boldoosw@gmail.com>
2024-10-13 11:26:07 +0800

    email regex pattern true bolgow

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -18,7 +18,8 @@ const Register = () => {
 
   const isValidEmail = (email: string) => {
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    return emailRegex.test(email);
+    // return emailRegex.test(email);
+    return true;
   };
   const handleSubmit = async (e: any) => {
     e.preventDefault();
